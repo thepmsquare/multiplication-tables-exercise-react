0744337c305450a4317519f1e3d92dfd80f0d955
thePmSquare <impmmmm@gmail.com>
2020-08-20 11:06:19 +0530

    Updates

---
+++ precache-manifest.db821898c7d5bad777af11c0a23ec188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2345cd3edbf49d80434b19cfe20d97f2",
+    "url": "/multiplication-tables-exercise-react/index.html"
+  },
+  {
+    "revision": "66bbc5fdab43b29be2d5",
+    "url": "/multiplication-tables-exercise-react/static/css/main.c055ef21.chunk.css"
+  },
+  {
+    "revision": "56df6686c7da0a6321a0",
+    "url": "/multiplication-tables-exercise-react/static/js/2.f06b46c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/multiplication-tables-exercise-react/static/js/2.f06b46c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bbc5fdab43b29be2d5",
+    "url": "/multiplication-tables-exercise-react/static/js/main.6e6bd9ea.chunk.js"
+  },
+  {
+    "revision": "32545ac2619d362b01dc",
+    "url": "/multiplication-tables-exercise-react/static/js/runtime-main.09e03c85.js"
+  }
+]);
